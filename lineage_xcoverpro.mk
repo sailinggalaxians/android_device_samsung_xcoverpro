f05eac81b311ce6fc2c249602c69589455a2f94d
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2021-09-08 16:05:46 +0200

    Corrected the build description

--- lineage_xcoverpro.mk
+++ lineage_xcoverpro.mk
@@ -1,19 +1,3 @@
-#
-# Copyright (C) 2018 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_q.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -35,11 +19,8 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-
-##TODO!! Build fingerprint needs to be changed
-
-BUILD_FINGERPRINT := samsung/xcoverpro/xcoverpro:11/QP1A.190711.020/G715FNXXS9CUH1:user/release-keys
+BUILD_FINGERPRINT := samsung/xcoverpro/xcoverpro:11/RP1A.200720.012 G715FNXXS9CUH1/G715FNXXS9CUH1:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=xcoverpro \
-        PRIVATE_BUILD_DESC="xcoverpro-user 11 QP1A.190711.020 G715FNXXS9CUH1 release-keys"
+        PRIVATE_BUILD_DESC="xcoverpro-user 11 RP1A.200720.012 G715FNXXS9CUH1 release-keys"
